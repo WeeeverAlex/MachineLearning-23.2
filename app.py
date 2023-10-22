f0afd636bc3128b29fc5c5c7971886d87af46c32
WeeverAlex <awever4000@gmail.com>
2023-10-22 18:56:55 -0300

    Fix Procfile and app name

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from joblib import load
+import os
 
 app = Flask(__name__)
 
@@ -26,5 +27,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
