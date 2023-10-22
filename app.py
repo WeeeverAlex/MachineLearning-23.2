b52e5ae161dd938c62c5644ee645909eb867d85d
WeeverAlex <awever4000@gmail.com>
2023-10-22 18:49:02 -0300

    Primeiro commit

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask, request, jsonify
-import joblib
+from joblib import load
 
 app = Flask(__name__)
 
-# Carregar o modelo
-model = joblib.load('elasticnet_model.pkl')
+# Carregar o modelo treinado
+model = load('elasticnet_model.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    prediction = model.predict([data['features']])
-    return jsonify(prediction=prediction[0])
+    try:
+        # Obter dados do JSON recebido
+        data = request.get_json(force=True)
+        
+        # Converter os dados em um dataframe (ajuste isso conforme sua necessidade)
+        # Aqui estou supondo que os dados vêm como um dicionário onde as chaves são os nomes das características e os valores são listas.
+        import pandas as pd
+        df = pd.DataFrame.from_dict(data)
+        
+        # Fazer previsão usando o modelo
+        predictions = model.predict(df)
+        
+        # Retornar as previsões em formato JSON
+        return jsonify(predictions.tolist())
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
